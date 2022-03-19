b5cbc921a20f5749a53b70a07fcb03a09c100cea
Philip M. Gollucci <pgollucci@p6m7g8.com>
2022-03-19 12:21:24 -0400

    chore: chases dependencie renames

--- init.zsh
+++ init.zsh
@@ -7,19 +7,8 @@
 ######################################################################
 p6df::modules::pagerduty::deps() {
   ModuleDeps=(
-    p6m7g8/p6common
+    p6m7g8-dotfiles/p6common
   )
-}
-
-######################################################################
-#<
-#
-# Function: p6df::modules::pagerduty::vscodes()
-#
-#>
-######################################################################
-p6df::modules::pagerduty::vscodes() {
-  true
 }
 
 ######################################################################
@@ -36,15 +25,4 @@ p6df::modules::pagerduty::langs() {
   p6_js_npm_global_install "pagerduty-cli"
 
   p6_github_util_org_repos_clone "PagerDuty" "$P6_DFZ_SRC_FOCUSED_DIR"
-}
-
-######################################################################
-#<
-#
-# Function: p6df::modules::pagerduty::init()
-#
-#>
-######################################################################
-p6df::modules::pagerduty::init() {
-  true
 }
