ea600b8b239035024b4e0ebeb7e9ebd1aae2a5b6
Philip M. Gollucci <pgollucci@p6m7g8.com>
2022-03-20 17:09:43 -0400

    chore: converts to new api (#2)

--- init.zsh
+++ init.zsh
@@ -23,6 +23,9 @@ p6df::modules::pagerduty::deps() {
 p6df::modules::pagerduty::langs() {
 
   p6_js_npm_global_install "pagerduty-cli"
+}
 
-  p6_github_util_org_repos_clone "PagerDuty" "$P6_DFZ_SRC_FOCUSED_DIR"
+p6df::modules::pagerduty::clones() {
+
+  p6_github_login_clone "PagerDuty" "$P6_DFZ_SRC_FOCUSED_DIR"
 }
