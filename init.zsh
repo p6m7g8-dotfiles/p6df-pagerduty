a2f31d941e073502e733bdaf1916d8729bf1753a
Philip M. Gollucci <pgollucci@p6m7g8.com>
2024-10-13 13:21:49 -0400

    feat: +p6df::modules::pagerduty::prompt::line

--- init.zsh
+++ init.zsh
@@ -15,20 +15,6 @@ p6df::modules::pagerduty::deps() {
 ######################################################################
 #<
 #
-# Function: p6df::modules::pagerduty::langs()
-#
-#>
-######################################################################
-p6df::modules::pagerduty::langs() {
-
-  p6_js_npm_global_install "pagerduty-cli"
-
-  p6_return_void
-}
-
-######################################################################
-#<
-#
 # Function: p6df::modules::pagerduty::clones()
 #
 #  Environment:	 P6_DFZ_SRC_FOCUSED_DIR
@@ -37,6 +23,28 @@ p6df::modules::pagerduty::langs() {
 p6df::modules::pagerduty::clones() {
 
   p6_github_login_clone "PagerDuty" "$P6_DFZ_SRC_FOCUSED_DIR"
+  p6_github_login_clone "PagerDuty-Samples" "$P6_DFZ_SRC_FOCUSED_DIR"
 
   p6_return_void
+}
+
+######################################################################
+#<
+#
+# Function: str str = p6df::modules::pagerduty::prompt::line()
+#
+#  Returns:
+#	str - str
+#
+#  Environment:	 PD_API_KEY
+#>
+######################################################################
+p6df::modules::pagerduty::prompt::line() {
+
+  local str="pagerduty:        pgollucci"
+  if ! p6_string_blank "$PD_API_KEY"; then
+    str="$str [api]"
+  fi
+
+  p6_return_str "$str"
 }
